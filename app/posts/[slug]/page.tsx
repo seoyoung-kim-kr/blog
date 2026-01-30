f47b974fd7e941c1291a9ebae44c5e9d8723e202
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-01-30 19:21:07 +0900

    feat : Detail Post Page 구현

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,54 @@
+import Container from "@/src/components/Container";
+import { getPost } from "@/src/service/posts";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
+import { FaRegCalendarAlt } from "react-icons/fa";
+
+type Props = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function DetailPostPage({ params }: Props) {
+  const { slug } = await params;
+  const post = await getPost({ slug });
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <Container>
+      <div className="bg-gray-50 space-y-4">
+        <div className="w-full relative h-64 overflow-hidden rounded-t-2xl">
+          <Image
+            src={`/images/posts/${slug}.png`}
+            alt="post-image"
+            fill
+            sizes="100vw"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+
+        <section className="px-4.5 py-3 space-y-4">
+          {/* Title, Date, Description */}
+          <div>
+            <div className="flex justify-between items-start space-y-1">
+              <h1 className="text-xl">{post.title}</h1>
+              <div className="text-sm flex gap-x-2 items-center">
+                <FaRegCalendarAlt />
+                {post.date}
+              </div>
+            </div>
+            <p>{post.description}</p>
+          </div>
+          <div className="bg-gray-200 w-full h-1" />
+          {/* Content */}
+          <ReactMarkdown>{post.content}</ReactMarkdown>
+        </section>
+      </div>
+    </Container>
+  );
+}
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,6 +10,10 @@ export type Post = {
   featured: boolean;
 };
 
+export type PostWithContent = Post & {
+  content: string;
+};
+
 export async function getPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), "data", "posts.json");
   const data = await fs.readFile(filePath, "utf-8");
@@ -26,4 +30,23 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
   const posts = await getPosts();
   const featuredPosts = posts.filter((post) => !post.featured);
   return featuredPosts;
+}
+
+export async function getPost({
+  slug,
+}: {
+  slug: string;
+}): Promise<PostWithContent | undefined> {
+  const posts = await getPosts();
+  const foundPost = posts.find((post) => post.path === slug);
+
+  if (!foundPost) return undefined;
+
+  const filePath = path.join(process.cwd(), "data/posts", `${slug}.md`);
+  const content = await fs.readFile(filePath, "utf-8");
+
+  return {
+    ...foundPost,
+    content,
+  };
 }
